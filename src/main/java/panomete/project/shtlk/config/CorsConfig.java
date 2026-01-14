48658337ccc4a10461b557b27d8b113b7de5e75c
oat431 <oat431@gmail.com>
2026-01-14 19:42:13 +0700

    add: cors for local dev

---
+++ src/main/java/panomete/project/shtlk/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package panomete.project.shtlk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final String[] allowedOrigins = {
+            "http://localhost:3000",
+            "http://localhost:5500"
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Content-Disposition", "Content-Length", "Content-Type", "Access-Control-Allow-Origin")
+                .allowCredentials(true);
+
+    }
+}
